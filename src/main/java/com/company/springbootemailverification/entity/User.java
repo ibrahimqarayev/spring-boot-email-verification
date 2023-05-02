ab247341a9bfa5c779c8d88b0a48e0da49e95762
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-02 15:28:20 +0400

    Added User entity

---
+++ src/main/java/com/company/springbootemailverification/entity/User.java
@@ -0,0 +1,30 @@
+package com.company.springbootemailverification.entity;
+
+import com.company.springbootemailverification.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    @NaturalId(mutable = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private boolean isEnabled = false;
+
+}
