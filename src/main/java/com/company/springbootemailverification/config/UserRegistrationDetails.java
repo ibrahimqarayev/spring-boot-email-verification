688d1f171847e7e0ea8b763c388eda2b788f0a88
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-03 23:35:05 +0400

    Added UserRegistrationDetails

---
+++ src/main/java/com/company/springbootemailverification/config/UserRegistrationDetails.java
@@ -0,0 +1,61 @@
+package com.company.springbootemailverification.config;
+
+import com.company.springbootemailverification.entity.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class UserRegistrationDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+    private boolean isEnabled;
+    private List<GrantedAuthority> grantedAuthorities;
+
+    public UserRegistrationDetails(User user) {
+        this.userName = user.getEmail();
+        this.password = user.getPassword();
+        this.isEnabled = user.isEnabled();
+        this.grantedAuthorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
