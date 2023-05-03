fa99a4aca646004e6e7c2beb8cdea736bce6687b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-03 11:25:02 +0400

    Added RegistrationCompleteEventListener

---
+++ src/main/java/com/company/springbootemailverification/event/listeneer/RegistrationCompleteEventListener.java
@@ -0,0 +1,32 @@
+package com.company.springbootemailverification.event.listeneer;
+
+import com.company.springbootemailverification.entity.User;
+import com.company.springbootemailverification.event.RegistrationCompleteEvent;
+import com.company.springbootemailverification.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
+    private final UserService userService ;
+
+    @Override
+    public void onApplicationEvent(RegistrationCompleteEvent event) {
+
+        User theUser = event.getUser();
+
+        String verificationToken = UUID.randomUUID().toString();
+
+        userService.saveUserVerificationToken(theUser,verificationToken);
+
+        String url = event.getApplicationUrl() + "/register/verify-email?token=" + verificationToken;
+
+        log.info("Click the link to verify your registration : {}" + url);
+    }
+}
