35e91834ee4665730366f2a40e25802969cf45ec
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-03 11:22:42 +0400

    Added RegistrationController

---
+++ src/main/java/com/company/springbootemailverification/registration/RegistrationController.java
@@ -0,0 +1,31 @@
+package com.company.springbootemailverification.registration;
+
+import com.company.springbootemailverification.entity.User;
+import com.company.springbootemailverification.event.RegistrationCompleteEvent;
+import com.company.springbootemailverification.registration.RegisterRequest;
+import com.company.springbootemailverification.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/register")
+public class RegistrationController {
+    private final UserService userService;
+    private final ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public String registerUser(RegisterRequest registerRequest, final HttpServletRequest request) {
+        User user = userService.registerUser(registerRequest);
+        publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
+        return "Success! Please , Check your email to complete the registration process";
+    }
+
+    public String applicationUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort()+request.getContextPath();
+    }
+}
