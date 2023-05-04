edbb60325fb7524dfaf6bb1bd47835c43d7b7a68
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-04 17:00:05 +0400

    Added method

--- src/main/java/com/company/springbootemailverification/registration/token/VerificationTokenRepository.java
+++ src/main/java/com/company/springbootemailverification/registration/token/VerificationTokenRepository.java
@@ -3,4 +3,5 @@ package com.company.springbootemailverification.registration.token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
 }
