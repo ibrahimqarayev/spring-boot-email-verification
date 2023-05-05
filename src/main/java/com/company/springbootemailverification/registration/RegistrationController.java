d707ca6994654d97bf045e0893ef9bf39e5350ba
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-05 08:44:23 +0400

    Completed RegistrationController

--- src/main/java/com/company/springbootemailverification/registration/RegistrationController.java
+++ src/main/java/com/company/springbootemailverification/registration/RegistrationController.java
@@ -3,13 +3,13 @@ package com.company.springbootemailverification.registration;
 import com.company.springbootemailverification.entity.User;
 import com.company.springbootemailverification.event.RegistrationCompleteEvent;
 import com.company.springbootemailverification.registration.RegisterRequest;
+import com.company.springbootemailverification.registration.token.VerificationToken;
+import com.company.springbootemailverification.registration.token.VerificationTokenRepository;
 import com.company.springbootemailverification.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,15 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @PostMapping
-    public String registerUser(RegisterRequest registerRequest, final HttpServletRequest request) {
+    public String registerUser(@RequestBody RegisterRequest registerRequest, final HttpServletRequest request) {
         User user = userService.registerUser(registerRequest);
         publisher.publishEvent(new RegistrationCompleteEvent(user, applicationUrl(request)));
         return "Success! Please , Check your email to complete the registration process";
     }
 
+    @GetMapping("/verify-email")
+    public String verifyEmail(@RequestParam("token") String token) {
+        VerificationToken theToken = verificationTokenRepository.findByToken(token);
+        if (theToken.getUser().isEnabled()) {
+            return "This account has already been verify , please login.";
+        }
+        String verificationResult = userService.validateToken(token);
+        if (verificationResult.equalsIgnoreCase("valid")) {
+            return "Email verified successfully . Now you can loin to account";
+        }
+        return "Invalid verification token";
+    }
+
+
     public String applicationUrl(HttpServletRequest request) {
-        return "http://" + request.getServerName() + ":" + request.getServerPort()+request.getContextPath();
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
 }
